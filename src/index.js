971bb4c21e7b1f260d07dbfe32561d03fb5cf303
lucky andreas <luckyandreas224@gmail.com>
2026-05-24 12:08:18 +0700

    feat: add upload

--- src/index.js
+++ src/index.js
@@ -5,6 +5,7 @@ const cors = require("cors");
 const authRoutes = require("./routes/auth.route");
 const itemRoutes = require("./routes/item.route");
 const userRoutes = require("./routes/user.route");
+const path = require("path");
 
 const app = express();
 
@@ -14,6 +15,7 @@ app.use(express.json());
 app.use("/api/auth", authRoutes);
 app.use("/api/items", itemRoutes);
 app.use("/api/users", userRoutes);
+app.use("/uploads", express.static(path.join(__dirname, "uploads")));
 
 app.listen(process.env.PORT, () => {
   console.log(`Server is running on port ${process.env.PORT}`);
--- src/routes/item.route.js
+++ src/routes/item.route.js
@@ -8,14 +8,16 @@ const {
   getItemById,
   updateItem,
 } = require("../controllers/item.controller");
+const { uploadSingle } = require("../middlewares/upload.middleware");
 
 const router = Router();
 
 router.get("/", getAllItems);
 router.get("/:itemId", getItemById);
 router.post("/:itemId/buy", authenticate, authorize("user"), buyItem);
-router.post("/", authenticate, authorize("admin"), createItem);
-router.put("/:itemId", authenticate, authorize("admin"), updateItem);
+router.post("/", authenticate, authorize("admin"), uploadSingle("image"), createItem);
+router.put("/:itemId", authenticate, authorize("admin"), uploadSingle("image"), updateItem);
 router.delete("/:itemId", authenticate, authorize("admin"), deleteItem);
+
 
 module.exports = router;
